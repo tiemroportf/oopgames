ca291c5e35dca33aaec545b5ab8daa1b5b587548
Tiemen <scaredrabbitnl@gmail.com>
2024-11-08 13:54:01 +0100

    T/M O4: 26

--- JS/H1/H1O04.js
+++ JS/H1/H1O04.js
@@ -6,7 +6,7 @@ function setup() {
 }
 
 function draw() {
-  // teken de grond
+  //teken de grond
   noStroke();
   fill('wheat');
   rect(0,350,450,100);
@@ -15,15 +15,17 @@ function draw() {
   stroke('darkgrey');
   fill('lightgray');
   rect(100,280,100,100);
+
   fill('gray');
-  // VERVANG DEZE REGEL door een regel die het dak tekent
+  triangle(100,280, 200,280,150,200)
   // teken de deur
   noStroke();
   // PAS ONDERSTAANDE REGEL AAN zodat de deur op de juiste plaats staat
-  rect(10,10,30,50);
+  rect(120,330,30,50);
   // teken de maan
   fill('khaki');
   // VERVANG DEZE REGEL door een regel die de maan tekent met een diameter van 150 en 25 vanaf de rand
+  ellipse(350,100,150,150)
   // teken de boom
   fill('sienna');
   rect(300,250,40,130);
@@ -31,4 +33,6 @@ function draw() {
   // VERVANG DEZE REGEL door een regel die het groen van de boom tekent met behulp van ellipse
   // Als je een cirkel wilt tekenen met diameter 300 kun je ellipse(0,0,300) gebruiken.
   // Als je een ellipse wilt maken met breedte 300 en hoogte 500, dan moet je een parameter toevoegen: ellipse(0,0,300,500).
+  ellipse(320,230,100,150);
+
 }
---
+++ JS/H1/commentaar.md
@@ -0,0 +1,2 @@
+H3O03.js:
+ - Opdracht 16: Dit komt omdat die rechthoek als eerste in de volgorde staat en dus niet zichtbaar is omdat de groene en blauwe rechthoeken over de rode heen zitten.
